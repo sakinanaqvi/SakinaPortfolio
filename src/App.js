e17683c1558a149acd8aeb548177c4295af04192
sakinanaqvi <82605626+sakinanaqvi@users.noreply.github.com>
2023-07-18 10:57:05 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './components/NavBar';
+import Header from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
+  // Available Colours:
+  // blue, cyan, gray, green, orange, pink, purple, red, teal, yellow
+
+  // edit this variable to change the color theme
+  const color = "teal";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav color={color} />
+      <Header color={color} />
+      <About color={color} />
+      <Experience color={color} />
+      <Projects color={color} />
+      <Contact color={color} />
+      <Footer />
+    </>
   );
 }
 
