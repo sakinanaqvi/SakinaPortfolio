9250c84fde40a28b022a46b446a50a87bf56f4b2
sakinanaqvi <82605626+sakinanaqvi@users.noreply.github.com>
2023-07-18 11:00:13 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ColorModeScript } from "@chakra-ui/react";
 import './index.css';
 import App from './App';
+import theme from "./theme";
 import reportWebVitals from './reportWebVitals';
+// 1. Import the extendTheme function
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App/>
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
